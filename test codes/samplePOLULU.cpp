96c3786029eb4d2fcc76038b093776ea01edf3a8
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-11-15 21:10:51 +0800

    Create samplePOLULU.cpp

---
+++ test codes/samplePOLULU.cpp
@@ -0,0 +1,18 @@
+#include <QTRSensors.h>
+
+QTRSensorsRC qtrrc((unsigned char[]) {2, 3, 4, 5, 6}, 5); // 5 sensors on pins 2-6
+unsigned int sensorValues[5];
+
+void setup() {
+    Serial.begin(9600);
+    qtrrc.calibrate(); // Calibrate the sensors
+}
+
+void loop() {
+    qtrrc.readCalibrated(sensorValues);
+    int position = qtrrc.readLine(sensorValues); // Get line position
+
+    Serial.print("Line Position: ");
+    Serial.println(position);
+    delay(100);
+}
