7d196be31345ed61e1cbe7f94b4677683102fe52
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-11-16 20:08:31 +0800

    Update l298n.cpp

--- test codes/motor/l298n.cpp
+++ test codes/motor/l298n.cpp
@@ -1,22 +1,28 @@
+// Motor pins connected to L298N
 #define IN1 12  // Motor 1 IN1
 #define IN2 14  // Motor 1 IN2
 #define IN3 27  // Motor 2 IN3
 #define IN4 26  // Motor 2 IN4
 
+// IR sensor pins
 #define LEFT_SENSOR_PIN 32  // Left sensor
 #define RIGHT_SENSOR_PIN 33  // Right sensor
 
+// Threshold for line detection
 #define THRESHOLD 1000
 
 void setup() {
+  // Set motor pins as outputs
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
+  // Set IR sensor pins as inputs
   pinMode(LEFT_SENSOR_PIN, INPUT);
   pinMode(RIGHT_SENSOR_PIN, INPUT);
 
+  // Start motors in stopped state
   stopMotors();
 }
 
@@ -24,17 +30,18 @@ void loop() {
   int leftSensorValue = digitalRead(LEFT_SENSOR_PIN);
   int rightSensorValue = digitalRead(RIGHT_SENSOR_PIN);
 
+  // Line following logic
   if (leftSensorValue == LOW && rightSensorValue == LOW) {
-    moveForward();
+    moveForward();  // Both sensors on the line, move forward
   }
   else if (leftSensorValue == LOW && rightSensorValue == HIGH) {
-    turnRight();
+    turnRight();  // Left sensor on the line, turn right
   }
   else if (leftSensorValue == HIGH && rightSensorValue == LOW) {
-    turnLeft();
+    turnLeft();  // Right sensor on the line, turn left
   }
   else {
-    stopMotors();
+    stopMotors();  // No sensor on the line, stop the robot
   }
 }
 
